3ffa608f7aa0fbd2fd2e6bfd287a8b20b9a46999
Matthias Glenk <glenkmatthias@gmail.com>
2024-08-08 21:47:04 +0200

    add enums

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/google/uuid"
 	"github.com/gorgoroth31/boulder-tracker/go-backend/models"
 )
@@ -11,9 +9,8 @@ func main() {
 	date := models.NewDate(22, 7, 2024)
 	time, _ := models.NewTime(16, 15)
 	time2, _ := models.NewTime(18, 15)
-	style, _ := models.GetStyleById(3)
-	fmt.Print(*style)
 
 	session := models.NewSession(uuid.New(), *date, *time, *time2, false)
 	session.Print()
+
 }
--- go-backend/models/boulder.go
+++ go-backend/models/boulder.go
@@ -1,20 +1,28 @@
 package models
 
+import (
+	"github.com/gorgoroth31/boulder-tracker/go-backend/enums/like"
+	"github.com/gorgoroth31/boulder-tracker/go-backend/enums/style"
+)
+
 type Boulder struct {
 	screwedDifficulty  Difficulty
 	feltLikeDifficulty Difficulty
 	attempts           int
 	sessionsTried      int
 	exhausting         bool
-	style              []Style
+	style              []style.Style
+	like               like.LikeType
 }
 
 func NewBoulder(screwedDifficulty Difficulty,
 	feltLikeDifficulty Difficulty,
 	attempts int, sessionsTried int,
-	exhausting bool, style []Style) *Boulder {
+	exhausting bool, style []style.Style,
+	like like.LikeType) *Boulder {
 	return &Boulder{screwedDifficulty: screwedDifficulty,
 		feltLikeDifficulty: feltLikeDifficulty,
 		attempts:           attempts, sessionsTried: sessionsTried,
-		exhausting: exhausting, style: style}
+		exhausting: exhausting, style: style,
+		like: like}
 }
