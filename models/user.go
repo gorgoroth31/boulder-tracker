5d1ae80649f2445518846697694892ca83752159
Matthias Glenk <matthias.glenk@aracom.de>
2025-07-30 14:50:28 +0200

    added user methods

--- models/user.go
+++ models/user.go
@@ -9,3 +9,28 @@ type User struct {
 	IsDeleted bool
 	Sessions  []Session
 }
+
+type UserDto struct {
+	Id        uuid.UUID `json:"id"`
+	UserName  string    `json:"userName"`
+	Email     string    `json:"email"`
+	IsDeleted bool      `json:"isDeleted"`
+}
+
+func (user *User) ToUserDto() *UserDto {
+	return &UserDto{
+		Id:        user.Id,
+		UserName:  user.UserName,
+		Email:     user.Email,
+		IsDeleted: user.IsDeleted,
+	}
+}
+
+func (user *UserDto) ToUserEntity() *User {
+	return &User{
+		Id:        user.Id,
+		UserName:  user.UserName,
+		Email:     user.Email,
+		IsDeleted: user.IsDeleted,
+	}
+}
