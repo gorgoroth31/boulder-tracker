93c133847e5aa9b242bdcd8654457e6baeda6cc4
Matthias Glenk <glenkmatthias@gmail.com>
2025-10-04 22:16:01 +0200

    add session WIP

--- boulder-tracker.client/src/views/AddSessionView.vue
+++ boulder-tracker.client/src/views/AddSessionView.vue
@@ -1,5 +1,19 @@
 ﻿<template>
   <div class="text-h5 gy-0">Wann warst du bouldern?</div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <DatePickerDialog title="Wann war die Session?" 
+                          :width="width" 
+                          header=""
+                          :max="new Date()"
+                          :height="height * 0.83"
+                          :date="session."
+                          >
+        </DatePickerDialog>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script setup lang="ts">
@@ -7,8 +21,12 @@ import {onMounted, Ref, ref} from "vue";
   import mainPageUtils from "./../utils/mainPageUtils";
   import {Session} from "node:inspector";
 import {getCurrentInProgressSession} from "@/api/session.api";
+import DatePickerDialog from "@/components/dialogs/DatePickerDialog.vue";
+import {useDisplay} from "vuetify/framework";
 
-  const session: Ref<Session | undefined> = ref();
+  const session: Ref<Session> = ref<Session>({});
+
+  const { width, height } = useDisplay()
   
   onMounted(() => {
     mainPageUtils.pageTitle.value = "Session hinzufügen";
